374daf0ce6500d5aa91d94c1a5f021a38a916753
Nakib Hossin <hussain0296@gmail.com>
2019-06-04 14:16:09 +0600

    Search and Dependencies for search is removed

--- app/components/dashboard/DashboardElements.js
+++ app/components/dashboard/DashboardElements.js
@@ -41,113 +41,11 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import NativeSelect from '@material-ui/core/NativeSelect';
 
-
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
 
 const drawerWidth = 240;
-const suggestions = [
-  { label: 'Afghanistan' },
-  { label: 'Aland Islands' },
-  { label: 'Albania' },
-  { label: 'Algeria' },
-  { label: 'American Samoa' },
-  { label: 'Andorra' },
-  { label: 'Angola' },
-  { label: 'Anguilla' },
-  { label: 'Antarctica' },
-  { label: 'Antigua and Barbuda' },
-  { label: 'Argentina' },
-  { label: 'Armenia' },
-  { label: 'Aruba' },
-  { label: 'Australia' },
-  { label: 'Austria' },
-  { label: 'Azerbaijan' },
-  { label: 'Bahamas' },
-  { label: 'Bahrain' },
-  { label: 'Bangladesh' },
-  { label: 'Barbados' },
-  { label: 'Belarus' },
-  { label: 'Belgium' },
-  { label: 'Belize' },
-  { label: 'Benin' },
-  { label: 'Bermuda' },
-  { label: 'Bhutan' },
-  { label: 'Bolivia, Plurinational State of' },
-  { label: 'Bonaire, Sint Eustatius and Saba' },
-  { label: 'Bosnia and Herzegovina' },
-  { label: 'Botswana' },
-  { label: 'Bouvet Island' },
-  { label: 'Brazil' },
-  { label: 'British Indian Ocean Territory' },
-  { label: 'Brunei Darussalam' },
-];
 
-function renderInputComponent(inputProps) {
-  const { classes, inputRef = () => {}, ref, ...other } = inputProps;
-
-  return (
-    <TextField
-      fullWidth
-      InputProps={{
-        inputRef: node => {
-          ref(node);
-          inputRef(node);
-        },
-        classes: {
-          input: classes.input,
-        },
-      }}
-      {...other}
-    />
-  );
-}
-
-function renderSuggestion(suggestion, { query, isHighlighted }) {
-  const matches = match(suggestion.label, query);
-  const parts = parse(suggestion.label, matches);
-
-  return (
-    <MenuItem selected={isHighlighted} component="div">
-      <div>
-        {parts.map((part, index) =>
-          part.highlight ? (
-            <span key={String(index)} style={{ fontWeight: 500 }}>
-              {part.text}
-            </span>
-          ) : (
-            <strong key={String(index)} style={{ fontWeight: 300 }}>
-              {part.text}
-            </strong>
-          ),
-        )}
-      </div>
-    </MenuItem>
-  );
-}
-
-function getSuggestions(value) {
-  const inputValue = deburr(value.trim()).toLowerCase();
-  const inputLength = inputValue.length;
-  let count = 0;
-
-  return inputLength === 0
-    ? []
-    : suggestions.filter(suggestion => {
-        const keep =
-          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-        if (keep) {
-          count += 1;
-        }
-
-        return keep;
-      });
-}
-
-function getSuggestionValue(suggestion) {
-  return suggestion.label;
-}
 
 const styles = theme => ({
   root: {
@@ -479,14 +377,7 @@ class DashboardElements extends Component {
     const { classes, theme } = this.props; //location doesnt work in this component!
 
     const { open , currentPath, anchorEl , anchorEl2} = this.state;
-    const autosuggestProps = {
-      renderInputComponent,
-      suggestions: this.state.suggestions,
-      onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
-      onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
-      getSuggestionValue,
-      renderSuggestion,
-    };
+    
 
     let dash = false;
     let pat = false;
@@ -591,38 +482,7 @@ class DashboardElements extends Component {
               <MenuIcon />
             </IconButton>
             <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <div style={{width:'85%',marginLeft:'8%'}}>
-              {/*<Autosuggest*/}
-                {/*{...autosuggestProps}*/}
-                {/*inputProps={{*/}
-                  {/*classes,*/}
-                  {/*placeholder: 'Search...',*/}
-                  {/*value: this.state.single,*/}
-                  {/*onChange: this.handleChange('single'),*/}
-                {/*}}*/}
-                {/*theme={{*/}
-                  {/*container: classes.container,*/}
-                  {/*suggestionsContainerOpen: classes.suggestionsContainerOpen,*/}
-                  {/*suggestionsList: classes.suggestionsList,*/}
-                  {/*suggestion: classes.suggestion,*/}
-                {/*}}*/}
-                {/*renderSuggestionsContainer={options => (*/}
-                  {/*<Paper {...options.containerProps} square>*/}
-                    {/*{options.children}*/}
-                  {/*</Paper>*/}
-                {/*)}*/}
-              {/*/>*/}
-              </div>
-              {/* <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              /> */}
+              
             </div>
             <div className={classes.sectionDesktop}>
               <IconButton
